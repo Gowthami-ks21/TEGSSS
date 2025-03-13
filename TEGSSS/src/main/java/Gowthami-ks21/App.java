ce34c43b1658a9db4a7a95ba2428466b3f625f2e
Gowthami-ks21 <gowthamiks77@gmail.com>
2025-03-13 15:26:43 +0530

    first commit

---
+++ TEGSSS/src/main/java/Gowthami-ks21/App.java
@@ -0,0 +1,13 @@
+package Gowthami-ks21;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
